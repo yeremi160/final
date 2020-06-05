5344579b4335690d7ed634bcb70c55e47f378906
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-04 19:22:05 -0600

    se agrego el primero boton

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ lblaño=Label(text="Año",font=("Agency FB",14)).place(x=80,y=150)
 entrada5=StringVar()
 txtaño=Entry(root,textvariable=entrada5,width=30).place(x=135,y=160)
 
-
+#botones
+btnFuncion1 = Button(root, text= "FUNCION 1",font=("Agency FB",10),width=10).place(x=74,y=180)
 
 
 
