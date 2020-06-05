a724306daf79d86eec17d7afe908674f036c2c21
Yeremi <yeremigonzalezyanes@gmail.com>
2020-06-04 18:55:25 -0600

    se agrego label nombre

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ alto = 250
 
 root.geometry(str(ancho)+"x"+str(alto))
 root.title("FINAL")
-
+#titulo
+saludo = Label(text="Bienvenido",font=("Agency FB",14)).place(x=190,y=5)
+#label
+lblnombre=Label(text="Nombre",font=("Agency FB",14)).place(x=80,y=30)
+entrada1=StringVar()
+txtnombre=Entry(root,textvariable=entrada1,width=30).place(x=135,y=40)
 
 
 
